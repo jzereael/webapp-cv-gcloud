57ed7e68cfd6f3997154579adee2834a25be0775
Jesse <jsebaltazar@gmail.com>
2020-04-13 18:42:17 -0600

    Added ejs for templates, body-parser, and lodash

--- app.js
+++ app.js
@@ -1,26 +1,66 @@
+//jshint esversion: 6
 'use strict';
-
 const express = require('express');
-const request = require('request');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+
 const app = express();
 
-// [START hello_world]
-// Say hello!
+//specify view engine
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + "/index.html");
+app.get('/', function(req, res) {
+  res.render('body', {
+    page_title: "Home"
+  });
 });
-// [END hello_world]
 
+
+app.get('/:custom_url', function(req, res) {
+  const custom_url = _.kebabCase(req.params.custom_url);
+  console.log(custom_url);
+
+  switch (custom_url) {
+
+    case "projects":
+      render_page(res, custom_url);
+      break;
+
+    case "blog":
+      render_page(res, custom_url);
+      break;
+
+    case "contact":
+      render_page(res, custom_url);
+      break;
+  }
+
+});
+
+
+
+function render_page(res, custom_url) {
+  console.log(`Rendering ${custom_url} page`);
+  res.render('body', {
+    page_title: _.capitalize(custom_url) +" Page"
+  });
+}
+
+
+
+
+
+// Check if Port declared otherwise use 8080
 if (module === require.main) {
-  // [START server]
-  // Start the server
   const server = app.listen(process.env.PORT || 8080, () => {
     const port = server.address().port;
     console.log(`App listening on port ${port}`);
   });
-  // [END server]
 }
-
 module.exports = app;
